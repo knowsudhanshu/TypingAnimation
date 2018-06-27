72de44fcad6b6503a49212117a39c5015b640eaf
Sudhanshu Sudhanshu <sudhanshu@sudhanshu-pro.local>
2018-06-27 14:22:07 +0530

    Initial Commit

---
+++ TypingAnimation/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TypingAnimation
+//
+//  Created by Sudhanshu Sudhanshu on 27/06/18.
+//  Copyright © 2018 Sudhanshu Sudhanshu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
